e912f708ec3ee93bbebdb3ba088dbc5f5dca78a6
Stanislau_Sukora <ssukora@estee.com>
2023-07-21 10:43:39 +0300

    Misc updates

--- src/main/java/school/mjc/stage0/conditions/task2/CoinFlip.java
+++ src/main/java/school/mjc/stage0/conditions/task2/CoinFlip.java
@@ -2,6 +2,19 @@ package school.mjc.stage0.conditions.task2;
 
 public class CoinFlip {
     public void throwCoin(int from1UpTo1000) {
+        if (from1UpTo1000 < 500) {
+            System.out.println("Eagle");
+        } else {
+            System.out.println("Tail");
+        }
+    }
 
+    public static void main(String[] args) {
+        CoinFlip coinFlip = new CoinFlip();
+
+        int numberToCheck = 725; // You can change this number as needed
+
+        System.out.println(numberToCheck);
+        coinFlip.throwCoin(numberToCheck);
     }
 }
--- src/main/java/school/mjc/stage0/conditions/task2/GreatestNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task2/GreatestNumberPrinter.java
@@ -2,5 +2,23 @@ package school.mjc.stage0.conditions.task2;
 
 public class GreatestNumberPrinter {
     public void printGreatest(int first, int second) {
+        if(first>second){
+            System.out.println(first);
+        }
+        else if (second>first){
+            System.out.println(second);
+        }
+        else {
+            System.out.println(first);
+        }
+    }
+
+    public static void main(String[] args) {
+        GreatestNumberPrinter printer = new GreatestNumberPrinter();
+
+        int first = 5;
+        int second = 6;
+
+        printer.printGreatest(first, second);
     }
 }
